14fb66a735131a6b5455811596502df1cc9da461
joseesteban_ochoa@outlook.com <joseesteban_ochoa@outlook.com>
2020-09-17 15:52:58 -0300

    fabrica2.8

--- src/controlador/ControladorRegistrarDefecto.java
+++ src/controlador/ControladorRegistrarDefecto.java
@@ -7,16 +7,22 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import vista.VistaPrincipalSupervisorCalidad;
+import vista.VistaRegistrarDefectos;
 
 /**
  *
  * @author Jose
  */
 public class ControladorRegistrarDefecto implements ActionListener {
-
+    VistaRegistrarDefectos vistaRegistrarDefectos;
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if(e.getActionCommand().equals(VistaPrincipalSupervisorCalidad.BTN_REALIZAR_INSPECCION)){
+            vistaRegistrarDefectos = new VistaRegistrarDefectos();
+            
+        }
     }
     
 }
